0781a29f6c7982938133165515f01d2c7db4297f
bermejoFlorence <152168210+bermejoFlorence@users.noreply.github.com>
2025-12-04 15:31:19 +0800

    font style

--- student/includes/header.php
+++ student/includes/header.php
@@ -17,6 +17,8 @@ $cssVer = file_exists($cssFs) ? filemtime($cssFs) : time();
 
   <script src="https://cdn.jsdelivr.net/npm/chart.js@4"></script>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+
   <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
   <link rel="stylesheet" href="<?php echo $cssRel . '?v=' . $cssVer; ?>">
 </head>
